6cbc9c74bba401c5e0331d2f346e71bde4d00c51
Gabriel Neves Neves <gabrielbangarclprates@gmail.com>
2023-05-12 18:23:01 -0400

    At

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+﻿function showItem(selector){
+    if(document.querySelector(selector) != undefined){
+        document.querySelector('.active').classList.remove('active');
+        console.log(selector)
+        document.querySelector(selector).classList.add('active');
+    }
+}
